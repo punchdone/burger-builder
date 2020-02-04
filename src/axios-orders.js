4773bc0743ba4a8a351cdec2c02f0faf1519f388
sreader <sreader@me.com>
2020-02-04 13:11:14 -0800

    Added redux

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://rw-burger.firebaseio.com/'
+    baseURL: 'https://rw-burger.firebaseio.com/'
 });
 
 export default instance;
